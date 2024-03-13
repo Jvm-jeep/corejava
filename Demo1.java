764add17f3d1e4d877338fba9162ab54b045700d
Sakshi Khatate <129989657+Jvm-jeep@users.noreply.github.com>
2024-03-13 15:44:17 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,25 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		List<Integer>l1=new ArrayList<Integer>();
+		
+		l1.add(30);
+		l1.add(20);
+		l1.add(10);
+		l1.add(50);
+		l1.add(40);
+		System.out.println(l1);
+		
+		System.out.println(l1.get(4));
+		
+		System.out.println(l1.indexOf(40));
+		
+		
+	}
+
+}
